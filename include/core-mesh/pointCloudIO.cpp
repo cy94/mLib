aa904cb0fcf115c443332aea5d210c83904c296e
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2022-08-23 12:41:18 +0200

    Support float and double vertices and normals in pointcloudIO::loadfromPLY

--- include/core-mesh/pointCloudIO.cpp
+++ include/core-mesh/pointCloudIO.cpp
@@ -25,79 +25,68 @@ namespace ml {
 			for (size_t i = 0; i < header.m_properties["vertex"].size(); i++) {
 				size += header.m_properties["vertex"][i].byteSize;
 			}
+			// create an empty array
 			char* data = new char[size*header.m_numVertices];
-		
+			// fill the array from file
 			file.read(data, size*header.m_numVertices);
-
+			// move data to the pointcloud object
 			for (unsigned long long i = 0; i < header.m_numVertices; i++) {
 				unsigned int byteOffset = 0;
 				const std::vector<PlyHeader::PlyPropertyHeader>& vertexProperties = header.m_properties["vertex"];
 
 				for (unsigned int j = 0; j < vertexProperties.size(); j++) {
+					char* data_ptr = &data[i * size + byteOffset];
 
-					auto readNextDecimalValue = [&]()
-					{
-						int byteSize = vertexProperties[j].byteSize;
-						FloatType f;
-						if (byteSize == 4)
-						{
-							f = (FloatType)((float*)&data[i*size + byteOffset])[0];
-						}
-						else if (byteSize == 8)
-						{
-							f = (FloatType)((double*)&data[i*size + byteOffset])[0];
-						}
-						byteOffset += byteSize;
-						return f;
-					};
+					// handle float/double vals for vertices and normals
+					// assume colors are always uchar
+					// TODO: handle any data type here dynamically
+					float val;
+
+					if (vertexProperties[j].nameType == "float") {
+						// get the float
+						val = ((float*) data_ptr)[0];
+					}
+					else if (vertexProperties[j].nameType == "double") {
+						// downcast double to float
+						val = static_cast<float>(((double*) data_ptr)[0]);
+					}
+					else if (vertexProperties[j].nameType == "uchar") {
+						// get the uchar, divide by 255 to normalize to 0-1 range and store in a float
+						val = ((unsigned char*)data_ptr)[0] / 255.0f;
+					}
 
 					if (vertexProperties[j].name == "x") {
-						pc.m_points[i].x = readNextDecimalValue();
-						//pc.m_points[i].x = ((float*)&data[i*size + byteOffset])[0];
-						//byteOffset += vertexProperties[j].byteSize;
+						pc.m_points[i].x = val;
 					}
 					else if (vertexProperties[j].name == "y") {
-						pc.m_points[i].y = readNextDecimalValue();
-						//pc.m_points[i].y = ((float*)&data[i*size + byteOffset])[0];
-						//byteOffset += vertexProperties[j].byteSize;
+						pc.m_points[i].y = val;
 					}
 					else if (vertexProperties[j].name == "z") {
-						pc.m_points[i].z = readNextDecimalValue();
-						//pc.m_points[i].z = ((float*)&data[i*size + byteOffset])[0];
-						//byteOffset += vertexProperties[j].byteSize;
+						pc.m_points[i].z = val;
 					}
 					else if (vertexProperties[j].name == "nx") {
-						pc.m_normals[i].x = ((float*)&data[i*size + byteOffset])[0];
-						byteOffset += vertexProperties[j].byteSize;
+						pc.m_normals[i].x = val;
 					}
 					else if (vertexProperties[j].name == "ny") {
-						pc.m_normals[i].y = ((float*)&data[i*size + byteOffset])[0];
-						byteOffset += vertexProperties[j].byteSize;
+						pc.m_normals[i].y = val;
 					}
 					else if (vertexProperties[j].name == "nz") {
-						pc.m_normals[i].z = ((float*)&data[i*size + byteOffset])[0];
-						byteOffset += vertexProperties[j].byteSize;
+						pc.m_normals[i].z = val;
 					}
 					else if (vertexProperties[j].name == "red") {
-						pc.m_colors[i].x = ((unsigned char*)&data[i*size + byteOffset])[0];	pc.m_colors[i].x/=255.0f;
-						byteOffset += vertexProperties[j].byteSize;
+						pc.m_colors[i].x = val;
 					}
 					else if (vertexProperties[j].name == "green") {
-						pc.m_colors[i].y = ((unsigned char*)&data[i*size + byteOffset])[0];	pc.m_colors[i].y/=255.0f;
-						byteOffset += vertexProperties[j].byteSize;
+						pc.m_colors[i].y = val;
 					}
 					else if (vertexProperties[j].name == "blue") {
-						pc.m_colors[i].z = ((unsigned char*)&data[i*size + byteOffset])[0];	pc.m_colors[i].z/=255.0f;
-						byteOffset += vertexProperties[j].byteSize;
+						pc.m_colors[i].z = val;
 					}
 					else if (vertexProperties[j].name == "alpha") {
-						pc.m_colors[i].w = ((unsigned char*)&data[i*size + byteOffset])[0];	pc.m_colors[i].w/=255.0f;
-						byteOffset += vertexProperties[j].byteSize;
-					} else {
-						//unknown (ignore)
-						byteOffset += vertexProperties[j].byteSize;
-					}
-
+						pc.m_colors[i].w = val;
+					} 
+					//unknown property -> ignore
+					byteOffset += vertexProperties[j].byteSize;
 				}
 				assert(byteOffset == size);
 
